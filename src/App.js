775e85127a1652635045bbd4881a6b3f38660a6a
Vishal Sharma <vishal.sharma@starkedge.com>
2023-03-20 15:13:45 +0530

    move 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-import Login from './component/login/Login';
-import Register from './component/register/Register';
-import Footer from './component/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Login from './component/login/Login';
+// import Register from './component/register/Register';
+// import Footer from './component/footer/Footer';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Usestate from './component/Usestate'
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      {/* <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
-      </BrowserRouter>
-      <Footer />     
+      </BrowserRouter> */}
+      {/* <Footer /> */}
+      <Usestate></Usestate>
     </div>
   );
 }
